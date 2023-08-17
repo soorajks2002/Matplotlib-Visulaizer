01a6ff39bca67860631fe44f228eb4fb6a7ad382
sooraj <soorajks2002@gmail.com>
2023-08-17 20:47:21 +0530

    Basic Pie Chart Plotting

---
+++ Pie_Chart.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import re
+import matplotlib.pyplot as plt
+
+collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
+
+numbers = st.text_input("PLease enter numbers")
+if numbers :
+    values = collect_numbers(numbers)
+
+    fig, axis = plt.subplots(1)
+    axis.pie(values)
+
+    st.pyplot(fig=fig)
