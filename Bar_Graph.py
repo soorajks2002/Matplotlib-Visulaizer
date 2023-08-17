0740c5c5ada37c3645f783275292d962095b35fe
sooraj <soorajks2002@gmail.com>
2023-08-17 20:53:46 +0530

    Basic Bar Graph

---
+++ Bar_Graph.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import re
+import matplotlib.pyplot as plt
+
+collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
+
+x = st.text_input("PLease enter x values")
+y = st.text_input("PLease enter y values")
+if x and y :
+    x = collect_numbers(x)
+    y = collect_numbers(y)
+
+    fig, axis = plt.subplots(1)
+    axis.bar(x,y)
+
+    st.pyplot(fig=fig)
